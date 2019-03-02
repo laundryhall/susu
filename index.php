2a6964d0aec6c540f92808ebdcd78d0bebf6b236
atsu <info@rvabookkeeping.com>
2019-03-02 16:35:21 -0500

    headbfffbvv

--- index.php
+++ index.php
@@ -104,7 +104,7 @@ $db->query('set @@session.time_zone = "-4:00"');
 		<div data-role="header" data-theme = "b" style = "height:40%;">
 			
 			<div data-role="navbar" style = "text-align:center">
-				<h1 onclick = "window.location.hash = '';location.reload();" style = "font-family:helvetica;">$U$U CARD</h1>
+				<p onclick = "window.location.hash = '';location.reload();" style = "font-family:helvetica;">SUSU CARD</p>
 				<ul>
 					<li id = "hiw"><a href="#">Home </a></li>
 					<li><a href="#" onclick = "window.location.hash = 'stayinformed'">How it works</a></li>
