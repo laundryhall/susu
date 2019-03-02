cb79b8c72d4b739429311c27c1400d25b2f24bd6
atsu <info@rvabookkeeping.com>
2019-03-02 14:36:29 -0500

    hegs

--- index.php
+++ index.php
@@ -119,9 +119,9 @@ $db->query('set @@session.time_zone = "-4:00"');
 			
 			<div style = 'color:black;font-weight:bold;font-size:45px;text-align:center;width:100%;height:100%;background-size:100% 100%;");'>
 				<br>
-				<img src="conceptImage.png" style = "width:20%;height:100px;">
+				<img src="conceptImage.png" style = "">
 				<br>
-				<p style = "vertical-align:middle;">PICKUP AND DELIVERY<br>FOR YOUR DRY CLEANING</p>
+				<p style = "vertical-align:middle;">THE ONLY COMMUNITY DISCOUNT CARD CUSTOMIZED FOR YOU</p>
 				<br><br>			
 			</div>
 			
